c9fbff62ad5bb055e2adac4ea547fcd3df1f77b7
onyxisland <dev.services@onyxisland.com>
2020-09-05 20:14:27 +0800

    Updates

---
+++ precache-manifest.09c7814e323158a92142ca6a3853891a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b9f638fec844c57f301c713293831b",
+    "url": "/gbd/index.html"
+  },
+  {
+    "revision": "858d51ca081f443f3c97",
+    "url": "/gbd/static/css/main.3cb90c10.chunk.css"
+  },
+  {
+    "revision": "3db93f3e18158bcd5646",
+    "url": "/gbd/static/js/2.9589cab8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gbd/static/js/2.9589cab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858d51ca081f443f3c97",
+    "url": "/gbd/static/js/main.22e70ab0.chunk.js"
+  },
+  {
+    "revision": "e3230cab698c20d88159",
+    "url": "/gbd/static/js/runtime-main.75307896.js"
+  },
+  {
+    "revision": "b086acded3501e233d9e6528cffc49fd",
+    "url": "/gbd/static/media/Mask-group-10.b086acde.png"
+  },
+  {
+    "revision": "5fa90ea585fe276a31dab73b193c0e43",
+    "url": "/gbd/static/media/Mask-group-2.5fa90ea5.png"
+  },
+  {
+    "revision": "724e06a538e07afd2947b55f50bd8910",
+    "url": "/gbd/static/media/Mask-group-3.724e06a5.png"
+  },
+  {
+    "revision": "de7b3a54d4e09bf27f757741bd7b17f1",
+    "url": "/gbd/static/media/Mask-group-4.de7b3a54.png"
+  },
+  {
+    "revision": "ae97e8250d66b8b7f75f365045c25f39",
+    "url": "/gbd/static/media/Mask-group-5.ae97e825.png"
+  },
+  {
+    "revision": "8c4b8e6ce06ce445b81fec98db37cec6",
+    "url": "/gbd/static/media/Mask-group-6.8c4b8e6c.png"
+  },
+  {
+    "revision": "d19e7716a08e6be0b22694b0f374369b",
+    "url": "/gbd/static/media/Mask-group-7.d19e7716.png"
+  },
+  {
+    "revision": "8c849bec118e862b90dbfe0083303b7f",
+    "url": "/gbd/static/media/Mask-group-8.8c849bec.png"
+  },
+  {
+    "revision": "ef47a37af41ab3878772d3a98de07014",
+    "url": "/gbd/static/media/Mask-group-9.ef47a37a.png"
+  },
+  {
+    "revision": "5f78b44a3abbba9bba0faf6a50330298",
+    "url": "/gbd/static/media/Mask-group.5f78b44a.png"
+  },
+  {
+    "revision": "8516d2589d71bb9be0ed470d79de4060",
+    "url": "/gbd/static/media/Rectangle-4.8516d258.png"
+  },
+  {
+    "revision": "9fd4151b6158ec9eb2b27b5a0ce9aa27",
+    "url": "/gbd/static/media/logo.9fd4151b.svg"
+  }
+]);
